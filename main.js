f8964d08b98bcb562555677049c6c21f85ba00dd
Maxim <maxim.anokhov@gmail.com>
2022-04-06 16:55:49 +0700

    refaktoring main.js

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
 import { buttonAddTask, buttonDeleteTask, buttonChangeStatus, } from "./view.js";
 import { setStorege, storageGetTask, tasksList } from "./storage.js"
+
 buttonAddTask.forEach((button) => button.addEventListener("click", getTaskName));
 buttonDeleteTask.forEach(button => button.addEventListener("click", deleteTask));
 buttonChangeStatus.forEach(button => button.addEventListener("click", changeStatus));
-const tasksHigh_list = document.querySelector(".high").lastElementChild.firstElementChild.nextElementSibling.textContent;
-const taskLow_list = document.querySelector(".low").lastElementChild.firstElementChild.nextElementSibling.textContent;
-
 
 function getTaskName(event) {
-    if (event) {
-        event.preventDefault();
-        const taskName = event.currentTarget.parentElement.firstElementChild.value;
-        const id = event.currentTarget.parentElement.parentElement.id;
-        console.log(id)
-        createTask(event, taskName, id)
-    } else {
-        console.log(taskLow_list, tasksHigh_list)
-    }
+
+    event.preventDefault();
+    const taskName = event.currentTarget.parentElement.firstElementChild.value;
+    const id = event.currentTarget.parentElement.parentElement.id;
+    createTask(taskName, id);
 }
 
-function createTask(event, taskName, id) {
+function createTask(taskName, id) {
 
     const html_element = `<div class="paragraf greenStyle">
         <input class="leftButton greenStyle" id="changeStatusTask" type="button">
@@ -33,21 +27,24 @@ function createTask(event, taskName, id) {
         this.priority = id;
         this.element = html_element;
     }
-    checkTask(task, event)
+    checkTask(task);
 }
 
-function checkTask(task, event) {
+function checkTask(task) {
+
     const addedTasksName = [];
     document.querySelectorAll(".content").forEach(item => addedTasksName.push(item.textContent));
-    const addedTask = addedTasksName.find(name => task.taskName == name)
+    const addedTask = addedTasksName.find(name => task.taskName == name);
 
     if (task.taskName !== addedTask) {
-        setStorege(task)
-        addTask(task, event);
-    }
+
+        setStorege(task);
+        addTask(task);
+
+    } else { alert("this task has already been added") }
 }
 
-function addTask(task, event, ) {
+function addTask(task) {
 
     const taskLow = document.querySelector(".low");
     const taskHigh = document.querySelector(".high");
@@ -59,35 +56,51 @@ function addTask(task, event, ) {
         taskHigh.insertAdjacentHTML("beforeend", task.element);
     }
     document.querySelectorAll(".leftButton").forEach(button => button.addEventListener("click", changeStatus));
-    document.querySelectorAll(".deleteTask").forEach(button => button.addEventListener("click", deleteTask))
+    document.querySelectorAll(".deleteTask").forEach(button => button.addEventListener("click", deleteTask));
 }
 
 function changeStatus(event) {
-    event.currentTarget.parentElement.classList.toggle('paragraf')
-    event.currentTarget.classList.toggle('greenStyle')
+
+    event.currentTarget.parentElement.classList.toggle('paragraf');
+    event.currentTarget.classList.toggle('greenStyle');
 }
 
 
 function deleteTask(event) {
+
     const deleteTask = event.currentTarget.previousElementSibling.textContent;
-    const newTaskList = [...tasksList].filter(item => item.taskName !== deleteTask)
+    const newTaskList = [...tasksList].filter(item => item.taskName !== deleteTask);
     event.currentTarget.parentElement.remove();
     tasksList.clear();
     localStorage.clear();
     newTaskList.forEach(task => {
-        setStorege(task)
+        setStorege(task);
     });
 
 }
 
 function startList() {
-    if (localStorage.getItem !== null) {
-        const tasks = storageGetTask()
 
+    const tasks = storageGetTask();
+    if (localStorage.getItem("task") != null) {
         tasks.forEach(task => {
             addTask(task);
             tasksList.add(task);
         });
     }
 }
-startList()
+startList();
+
+import { compareAsc, format } from 'date-fns';
+
+
+format(new Date(2022, 4, 5), "yyyy,MM,dd");
+
+const dates = [
+
+    new Date(1995, 6, 2),
+    new Date(1987, 3, 12),
+    new Date(1989, 6, 18),
+]
+dates.sort(compareAsc);
+console.log(dates);
